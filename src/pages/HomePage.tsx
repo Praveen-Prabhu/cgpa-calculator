7d1ba3839a7735be7eef160809f17dc2b019556e
Praveen Prabhu <praveenzues@gmail.com>
2025-07-25 11:55:25 +0530

    Add login page

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const HomePage: React.FC = () => {
+  return (
+    <div className="text-center text-2xl font-semibold mt-10">Welcome</div>
+  );
+};
+
+export default HomePage;
