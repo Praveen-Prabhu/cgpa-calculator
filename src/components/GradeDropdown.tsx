24bc5a02c0a2fad0892e53f84ceff7e919c4b75a
KavyaSwethaJ <j.kavyaswetha@gmail.com>
2025-07-25 12:24:05 +0530

    Modified components to take in json data

--- src/components/GradeDropdown.tsx
+++ src/components/GradeDropdown.tsx
@@ -4,13 +4,23 @@ interface GradeDropdownProps {
   value: string;
   onChange: (newGrade: string) => void;
   disabled?: boolean;
+  id?: string;       
+  name?: string;     
 }
 
-const grades = ["O","A+", "A", "B+", "B", "U"];
+const grades = ["O", "A+", "A", "B+", "B", "U"];
 
-const GradeDropdown: React.FC<GradeDropdownProps> = ({ value, onChange, disabled = false, }) => {
+const GradeDropdown: React.FC<GradeDropdownProps> = ({
+  value,
+  onChange,
+  disabled = false,
+  id = "grade-select",
+  name = "grade",
+}) => {
   return (
     <select
+      id={id}
+      name={name}
       className="w-26 px-3 py-2 text-sm rounded-md bg-base-200 text-base-content focus:outline-none focus:ring-0 border-none"
       value={value}
       onChange={(e) => onChange(e.target.value)}
